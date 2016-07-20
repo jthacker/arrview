07fa8dee758bd7cda38e5a1847664767ab696f4c
jthacker <thacker.jon@gmail.com>
2016-07-20 17:32:47 -0500

    prepare for public release

--- arrview/__init__.py
+++ arrview/__init__.py
@@ -1,8 +1,9 @@
 import os
 os.environ['ETS_TOOLKIT'] = 'qt4'
 
-from .main import ArrayViewer
-from .slicer import Slicer
+from arrview.version import __version__
+from arrview.main import ArrayViewer
+from arrview.slicer import Slicer
 
 def view(arr, roi_filename=None, rois_updated=None, title=None):
     viewer = ArrayViewer(Slicer(arr),
--- setup.py
+++ setup.py
@@ -1,11 +1,29 @@
-from distutils.core import setup
+from setuptools import setup
 
-setup(
-        name='arrview',
-        version='0.1dev',
-        packages=['arrview', 'arrview.ui'],
-        install_requires=[
-            'numpy','traits','traitsui',
-            'scikit-image','h5py',
-            'matplotlib', 'PySide']
-)
+# Load __version__ without importing it (avoids race condition with build)
+exec(open('arrview/version.py').read())
+
+setup(name='arrview',
+      description='An N-dimensional array viewer',
+      author='jthacker',
+      author_email='thacker.jon@gmail.com',
+      version=__version__,
+      url='https://github.com/jthacker/arrview',
+      packages=['arrview', 'arrview.ui'],
+      install_requires=[
+          'numpy',
+          'traits',
+          'traitsui',
+          'scipy',
+          'scikit-image',
+          'h5py',
+          'matplotlib',
+          'jtmri',
+          'PySide'],
+     tests_require=[
+          'pytest'
+          ],
+      setup_requires=[
+          'pytest-runner'
+          ],
+      )
